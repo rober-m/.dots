a956ca83d7f9534deaf15991a208eccf67bc9969
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2022-12-10 22:58:49 +0000

    extracted custom-launcher and added dummy ghci config

---
+++ modules/custom-launcher.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, lib, ... }:
+{
+  # TODO: Create "Quick Service" running "customLauncher.sh" and add Cmd+Space shortcut to it
+  home.file.customLauncher = {
+    executable = true;
+    target = ".config/scripts/customLauncher.sh";
+    text = ''
+      #!/bin/zsh
+
+      ~/.config/scripts/popout.sh ~/.config/scripts/launcher.sh
+    '';
+  };
+
+  home.file.launcherPopoup = {
+    executable = true;
+    target = ".config/scripts/popoup.sh";
+    text = ''
+      #!/bin/zsh
+
+      # Set terminal with "mylauncher" tytle as a floating window in Amethyst/Yabai
+      alacritty -t "mylauncher" -o window.decorations=none --working-directory "$(pwd)" -e "$1"
+    '';
+  };
+
+  home.file.launcherBehavior = {
+    executable = true;
+    target = ".config/scripts/launcher.sh";
+    text = ''
+      #!/bin/zsh
+
+      find -L \
+          ~/Applications/Home\ Manager\ Apps \
+          /System/Library/CoreServices \
+          /System/Applications /Applications \
+          /System/Applications/Utilities \
+          ~/Applications \
+          -maxdepth 1 -name "*.app" | fzf | xargs -I {} open "{}" 
+    '';
+  };
+}

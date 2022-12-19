4d5f9765d69947f60b9189f8d90dcdb08945e0d6
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2022-12-19 21:21:40 +0000

    fixed nvim-cmp error

--- modules/neovim/nvim-cmp.nix
+++ modules/neovim/nvim-cmp.nix
@@ -42,22 +42,6 @@
             -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
             --   capabilities = capabilities
             -- }
-            formatting = {
-              format = function(entry, vim_item)
-                if vim.tbl_contains({ 'path' }, entry.source.name) then
-                  local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-                  if icon then
-                    vim_item.kind = icon
-                    vim_item.kind_hl_group = hl_group
-                    return vim_item
-                  end
-                end
-                return lspkind.cmp_format({ with_text = false })(entry, vim_item)
-              end
-            }
-            experimental = {
-              ghost-text = true
-            }
           })
           ----------------------------------------------------------------------------------------- 
         '';

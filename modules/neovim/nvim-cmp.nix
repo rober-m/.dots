c6e11cf3c525895277a7f365c4e6f0217f3879da
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-06 23:13:42 +0000

    solved extraPackages nvim plugins don't load

--- modules/neovim/nvim-cmp.nix
+++ modules/neovim/nvim-cmp.nix
@@ -3,13 +3,26 @@
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
+
+      # COMPLETION PROVIDERS
       cmp-nvim-lsp # Completes with LSP server (Auto import, snippets, etc.)
+      cmp-path # Completes files
+      cmp-nvim-lua # Completes with Lua and Neovim keywords
+      cmp-buffer # Completes with word on the current buffer
+      luasnip # Snippet Engine for Neovim written in Lua. (there are other options).
+      cmp_luasnip # Completes with snippets from luasnip
+
+      # ICONS
+      nvim-web-devicons # Adds icons to my pluggins
+
+      # MAIN CONFIG
       {
         # This pluging provides an engine to have suggested competions based
         # on the completion providers (see extraPackages below)
         plugin = nvim-cmp;
         #type = "lua";
         config = ''
+          " Start of NVIM-CMP config
           set completeopt=menu,menuone,noselect
           lua << EOF
           ---------------------------------- NVIM-CMP ---------------------------------------
@@ -50,21 +63,6 @@
           EOF
         '';
       }
-    ];
-
-    extraPackages = with pkgs.vimPlugins; [
-
-      # COMPLETION PROVIDERS
-      cmp-nvim-lsp # Completes with LSP server (Auto import, snippets, etc.)
-      cmp-path # Completes files
-      cmp-nvim-lua # Completes with Lua and Neovim keywords
-      cmp-buffer # Completes with word on the current buffer
-      luasnip # Snippet Engine for Neovim written in Lua. (there are other options).
-      cmp_luasnip # Completes with snippets from luasnip
-
-      # ICONS
-      nvim-web-devicons # Adds icons to my pluggins
-
     ];
 
     # extraConfig goes in a .vim file, not .lua!

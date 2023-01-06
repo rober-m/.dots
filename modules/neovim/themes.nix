07d31a05f442797efa00b87e17aad4ca4f4bc51d
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-06 23:03:03 +0000

    played around with themes

--- modules/neovim/themes.nix
+++ modules/neovim/themes.nix
@@ -3,20 +3,47 @@
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       sonokai
-      dracula-vim
+      dracula-nvim
       gruvbox
       papercolor-theme
+
+      # TODO: The only themes that correctly works with transparent BG is gruvbox and papercolor
 
       {
         plugin = tokyonight-nvim;
         type = "lua";
         config = ''
           ------------------------------- THEMES: TOKYO NIGHT ------------------------------
-          -- vim.g.tokyonight_transparent = true
-          -- vim.g.tokyonight_transparent_sidebar = true
+          vim.g.tokyonight_transparent = true
+          vim.g.tokyonight_transparent_sidebar = true
           require("tokyonight").setup({
             -- See options here: https://github.com/folke/tokyonight.nvim
-            -- transparent = true, -- Enable this to disable setting the background color
+            transparent = true, -- Enable this to disable setting the background color
+          })
+          -----------------------------------------------------------------------------------
+        '';
+      }
+
+      {
+        plugin = onedark-nvim;
+        type = "lua";
+        config = ''
+          ------------------------------- THEMES: ONEDARK ------------------------------
+          require("onedark").setup({
+            -- See options here: https://github.com/navarasu/onedark.nvim/
+            transparent = true, -- Show/hide background
+            style = 'dark', -- Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+            -- Lualine options --
+            lualine = {
+                transparent = false, -- lualine center bar transparency
+            },
+            -- Plugins Config --
+            diagnostics = {
+                darker = true, -- darker colors for diagnostic
+                undercurl = true,   -- use undercurl instead of underline for diagnostics
+                background = true,    -- use background color for virtual text
+            },
+
           })
           -----------------------------------------------------------------------------------
         '';

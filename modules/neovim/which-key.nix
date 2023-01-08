1f8d729c21e582449b3408e0088b735cd1bcd2ef
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-08 22:08:35 +0000

    added two movement keybindings

--- modules/neovim/which-key.nix
+++ modules/neovim/which-key.nix
@@ -9,10 +9,15 @@
 
           ------------------------------------- WHICH-KEY -----------------------------------------
           require("which-key").setup {
+
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
           }
+          require("which-key").register({
+            ["gh"] = { "0", "GoTo beginning of line"},
+            ["gl"] = { "$", "GoTo end of line"},
+          })
           -----------------------------------------------------------------------------------------
         '';
       }

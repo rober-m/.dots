fbde1ae45fd40e64ac69f94e3f1f4fc61e247ae2
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-14 23:41:14 +0000

    added bufferline config

--- modules/neovim/bufferline.nix
+++ modules/neovim/bufferline.nix
@@ -3,18 +3,44 @@
 {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
-    # Buffer tabs
+      # Buffer tabs
       {
         plugin = bufferline-nvim;
         type = "lua";
         config = ''
-          require("bufferline").setup{ }
-          nmap("<leader>b", ":BufferLineCycleNext<cr>")
-          nmap("<leader>B", ":BufferLineCyclePrev<cr>")
-          nmap("<leader>c", ":bd<cr>")
+          ---------------------------------- NVIM-LSPCONFIG ---------------------------------------
+          -- Docs: https://github.com/akinsho/bufferline.nvim
+          -- You can close buffers by clicking the close icon or by right clicking the tab anywhere
+          require("bufferline").setup{
+            options = {
+              diagnostics = "nvim_lsp",
+              diagnostics_indicator = function(count, level, diagnostics_dict, context)
+	        local s = " "
+	        for e, n in pairs(diagnostics_dict) do
+		  local sym = e == "error" and " "
+		    or (e == "warning" and " " or "" )
+		  s = s .. n .. sym
+	        end
+	        return s
+	      end
+            }
+          }
+
+          require("which-key").register({
+                                b = {
+                                  name = "Buffer",
+                                  h = { ":BufferLineCycleNext<cr>", "Cycle Next" },
+                                  l = { ":BufferLineCyclePrev<cr>", "Cycle Prev" },
+                                  c = { ":bd<cr>", "Close" },
+                                  C = { ":bd!<cr>", "Close without saving" },
+                                },
+                              }, { prefix = "<leader>" })
+
+          ----------------------------------------------------------------------------------------- 
         '';
       }
     ];
 
   };
 }
+

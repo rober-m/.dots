e7682a2f817c28874f62d7d40f4778a92be1fefc
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-17 21:57:21 +0000

    auto-close nvim-tree

--- modules/neovim/nvim-tree.nix
+++ modules/neovim/nvim-tree.nix
@@ -23,6 +23,17 @@
           -- Remappings
           nmap("<C-e>", ":NvimTreeToggle<cr>")
           imap("<C-e>", ":NvimTreeToggle<cr>")
+
+          -- Hack to close nvim-tree when last buffer. See:
+          -- https://github.com/nvim-tree/nvim-tree.lua/issues/1368#issuecomment-1195557960
+          vim.api.nvim_create_autocmd("BufEnter", {
+            group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+            pattern = "NvimTree_*",
+            callback = function()
+              local layout = vim.api.nvim_call_function("winlayout", {})
+              if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+            end
+          })
           ----------------------------------------------------------------------------------------- 
         '';
       }

fa5918a3c8cff150eb6d24c5aa28f5eee3e1369e
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-01-19 19:27:54 +0000

    -

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,12 @@
   nix.settings.substituters = [
     "https://cache.nixos.org/" # Default NixOS cache
     "https://nixcache.reflex-frp.org" # Obelisk cache
+    "https://cache.iog.io" # IOG (IOHK) binary cache
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # Default NixOS cache key (I think)
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" # Obelisk cache key
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" # IOG (IOHK) binary cache
   ];
   nix.settings.trusted-users = [
     "@admin"

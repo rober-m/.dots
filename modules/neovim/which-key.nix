34aee9ade75bc32a72da931d2150aeddfb614b81
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-02-01 11:39:29 +0000

    improved pane navigation

--- modules/neovim/which-key.nix
+++ modules/neovim/which-key.nix
@@ -18,6 +18,23 @@
             ["gh"] = { "0", "GoTo beginning of line"},
             ["gl"] = { "$", "GoTo end of line"},
           })
+
+          local normal_pane_movements = {
+              ["∆"] = { "<C-w>j", "Move to pane below"},
+              ["˚"] = { "<C-w>k", "Move to pane above"},
+              ["˙"] = { "<C-w>h", "Move to left pane"},
+              ["¬"] = { "<C-w>l", "Move to right pane"},
+          }
+
+          local terminal_pane_movements = {
+              ["∆"] = { "<C-\\><C-N><C-w>j", "Move to pane below"},
+              ["˚"] = { "<C-\\><C-N><C-w>k", "Move to pane above"},
+              ["˙"] = { "<C-\\><C-N><C-w>h", "Move to left pane"},
+              ["¬"] = { "<C-\\><C-N><C-w>l", "Move to right pane"},
+          }
+
+          require("which-key").register(normal_pane_movements, { mode = 'n', silent = true })
+          require("which-key").register(terminal_pane_movements, { mode = 't', silent = true })
           -----------------------------------------------------------------------------------------
         '';
       }

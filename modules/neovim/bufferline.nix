c532dac658c2038f1f6faf67a04101c495c11f8d
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-02-01 12:58:52 +0000

    fixed buffer movement

--- modules/neovim/bufferline.nix
+++ modules/neovim/bufferline.nix
@@ -8,29 +8,30 @@
         plugin = bufferline-nvim;
         type = "lua";
         config = ''
-          ---------------------------------- NVIM-LSPCONFIG ---------------------------------------
+          ---------------------------------- BUFFERLINE-NVIM --------------------------------------
           -- Docs: https://github.com/akinsho/bufferline.nvim
           -- You can close buffers by clicking the close icon or by right clicking the tab anywhere
-          require("bufferline").setup{
+
+          require("bufferline").setup {
             options = {
               diagnostics = "nvim_lsp",
               diagnostics_indicator = function(count, level, diagnostics_dict, context)
-	        local s = " "
-	        for e, n in pairs(diagnostics_dict) do
-		  local sym = e == "error" and " "
-		    or (e == "warning" and " " or "" )
-		  s = s .. n .. sym
-	        end
-	        return s
-	      end
+                local s = " "
+                for e, n in pairs(diagnostics_dict) do
+                  local sym = e == "error" and " "
+                      or (e == "warning" and " " or "")
+                  s = s .. n .. sym
+                end
+                return s
+              end
             }
           }
 
           require("which-key").register({
                                 b = {
                                   name = "Buffer",
-                                  h = { ":BufferLineCycleNext<cr>", "Cycle Next" },
-                                  l = { ":BufferLineCyclePrev<cr>", "Cycle Prev" },
+                                  l = { ":BufferLineCycleNext<cr>", "Cycle Next" },
+                                  h = { ":BufferLineCyclePrev<cr>", "Cycle Prev" },
                                   c = { ":bd<cr>", "Close" },
                                   C = { ":bd!<cr>", "Close without saving" },
                                 },

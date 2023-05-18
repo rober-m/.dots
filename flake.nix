31017090c0884041e9574b7d233c7d6d71aff648
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-05-18 14:08:36 +0100

    organized a bit flake.nix and configuration.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
 {
-  description = "Rober's darwin system";
-  # TODO
-  # - Organize better
+  description = "Robertino's darwin system";
+
+  ###############################################################################################
+  ######################################  INPUTS ################################################
 
   inputs = {
     # Package sets
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     #nixpkgs-with-patched-kitty.url = github:azuwis/nixpkgs/kitty;
 
     # Environment/system management
@@ -16,16 +17,18 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # Other sources
-    comma = { url = github:Shopify/comma; flake = false; };
+    comma = { url = "github:Shopify/comma"; flake = false; };
     
     nixvim.url = "github:pta2002/nixvim";
   };
 
+  ###############################################################################################
+  ###################################### OUTPUTS ################################################
+
   outputs = { self, darwin, nixpkgs, home-manager, nixvim, ... }@inputs:
   let 
 
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs-unstable.lib) attrValues makeOverridable optionalAttrs singleton;
+    inherit (inputs.nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
 
     # Configuration for `nixpkgs`
     nixpkgsConfig = {
@@ -39,13 +42,14 @@
         })
       );
     }; 
+
   in
   {
 
     # My `nix-darwin` configs
     darwinConfigurations."roberm" = darwin.lib.darwinSystem {
         system = "aarch64-darwin";
-        modules = attrValues self.darwinModules ++ [ 
+        modules = [ 
           # Main `nix-darwin` config
           ./configuration.nix
           # `home-manager` module
@@ -60,40 +64,15 @@
         ];
       };
 
-    # Overlays --------------------------------------------------------------- {{{
-
     overlays = {
-      # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs-unstable {
-            system = "x86_64-darwin";
-            inherit (nixpkgsConfig) config;
-          };
-        }; 
-      };
-
-    # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
-    # fixes.
-    darwinModules = {
-      programs-nix-index = 
-        # Additional configuration for `nix-index` to enable `command-not-found` functionality with Fish.
-        { config, lib, pkgs, ... }:
-
-        {
-          config = lib.mkIf config.programs.nix-index.enable {
-            programs.fish.interactiveShellInit = ''
-              function __fish_command_not_found_handler --on-event="fish_command_not_found"
-                ${if config.programs.fish.useBabelfish then ''
-                command_not_found_handle $argv
-                '' else ''
-                ${pkgs.bashInteractive}/bin/bash -c \
-                  "source ${config.progams.nix-index.package}/etc/profile.d/command-not-found.sh; command_not_found_handle $argv"
-                ''}
-              end
-            '';
-            };
-        };
+       # Overlay useful on Macs with Apple Silicon
+       apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+           # Add access to x86 packages system is running Apple Silicon
+           pkgs-x86 = import inputs.nixpkgs-unstable {
+             system = "x86_64-darwin";
+             inherit (nixpkgsConfig) config;
+           };
+       }; 
     };
  };
 }
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 let
   imports = [
     ./modules/neovim

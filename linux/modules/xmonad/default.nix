edce0cde5e060a4b161a65e90033fd1a1ebe3960
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-10-15 15:31:14 +0100

    added empty XMonad config

--- linux/modules/xmonad/default.nix
+++ linux/modules/xmonad/default.nix
@@ -1,17 +1,29 @@
-{ xmonad-contexts, ... }:
+# Docs:
+# - https://nixos.wiki/wiki/XMonad
+# TODO: Create a project around xmonad.hs (See las section of previous link)
+{ ... }:
 {
   services.xserver.windowManager.xmonad = {
     enable = true;
-    enableContribAndExtras = true;
+    config = builtins.readFile ./xmonad.hs;
+
+    # Adds the xmonad-contrib and xmonad-extras packages
+    # https://hackage.haskell.org/package/xmonad-contrib
+    enableContribAndExtras = true; 
+
+    # When developing modules for XMonad, it can help to install the following packages: 
     extraPackages = haskellPackages: [
       haskellPackages.dbus
       haskellPackages.List
       haskellPackages.monad-logger
     ];
+
     ghcArgs = [
       "-hidir /tmp" # place interface files in /tmp, otherwise ghc tries to write them to the nix store
       "-odir /tmp" # place object files in /tmp, otherwise ghc tries to write them to the nix store
-      "-i${xmonad-contexts}" # tell ghc to search in the respective nix store path for the module
+
+      # To add Haskell modules that are not included in the Xmonad-Contrib package, you have to tell ghc where to find them.
+      #"-i${xmonad-contexts (only an example)}" # tell ghc to search in the respective nix store path for the module
     ];
   };
 }

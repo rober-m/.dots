c2e5a8123c12022013d9852a0d46c3cef48e6599
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-10-22 16:08:33 +0100

    test 2

--- common/modules/zsh.nix
+++ common/modules/zsh.nix
@@ -45,9 +45,7 @@
       # export KEYTIMEOUT=1 # Needed for ZSH vi mode
       VI_MODE_RESET_PROMPT_ON_MODE_CHANGE=true # For oh-my-zsh vi-mode
       eval "$(direnv hook zsh)" # Needed for direnv
-
-      # Should I add evaluation of SSH agent?
-      # eval "$(ssh-agent -s)"
+      eval "$(ssh-agent -s)" # Initialize SSH agent
     '';
 
     sessionVariables = {

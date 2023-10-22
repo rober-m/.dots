eb003876f9b3a4c8b1038c8357160c3f910f73c1
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-10-22 15:49:28 +0100

    remove custom vscode-linux and megaSync (doesn't compile anymore)

--- linux/home.nix
+++ linux/home.nix
@@ -4,7 +4,7 @@
     # Start of Linux specific configuration
     ./modules/zsh.nix
     #./modules/gui/gtk.nix
-    ./modules/vscode-linux.nix
+    #./modules/vscode-linux.nix
     ./modules/firefox.nix
   ];
 in {
@@ -20,10 +20,12 @@ in {
   home.packages = with pkgs; [
     protonvpn-gui # Proton VPN
     bitwarden # Password manager
-    megasync # MegaSync (MEGA Cloud Drive)
+    #megasync # MegaSync (MEGA Cloud Drive) # TODO: Check if this was solved: https://github.com/NixOS/nixpkgs/issues/126848
     telegram-desktop # Telegram for Linux
     emote # emoji picker
     variety # Wallpaper manager
     google-chrome # Google Chrome browser
+    # HLS 2.2.0 doesn't work in MacOS :(
+    (haskell-language-server.override {supportedGhcVersions = ["810" "925" "928"];}) #Also installed in modules/nvim/lsp-config.nix
   ];
 }
--- linux/modules/vscode-linux.nix
+++
@@ -1,9 +0,0 @@
-# Linux-specific config on top of `common/modules/vscode`
-{pkgs, ...}: {
-  #TODO: Add language servers (the ones in the nvim config) here? Add them to home.nix? Let VSCode manage the installation?
-  programs.vscode.extensions = {
-    extensions = with pkgs.vscode-extensions; [
-      ms-vsliveshare.vsliveshare # Live Share (https://marketplace.visualstudio.com/items?itemName=ms-vsliveshare.vsliveshare)
-    ];
-  };
-}

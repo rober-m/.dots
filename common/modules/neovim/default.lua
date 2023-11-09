63472db16922326616a6a4bb1988c0c5c45e9c46
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2023-11-09 11:44:23 +0000

    reformat nvim config

--- common/modules/neovim/default.lua
+++ common/modules/neovim/default.lua
@@ -25,20 +25,21 @@ vim.opt.wrap = false -- Wrap text?
 vim.opt.cursorline = true -- Highlight cursor line
 vim.opt.scrolloff = 15 -- Screen lines above and below cursor
 vim.opt.list = false -- List mode: Show invisible chararcters
-vim.opt.listchars = "eol:¬,tab:>·,extends:>,precedes:<,space:·" -- Specify how to show invisible chars (`list = true` needed)
+vim.opt.listchars =
+"eol:¬,tab:>·,extends:>,precedes:<,space:·" -- Specify how to show invisible chars (`list = true` needed)
 
 -- Numbers
-vim.opt.number = true -- Show line numbers
+vim.opt.number = true         -- Show line numbers
 vim.opt.relativenumber = true -- Use relative line numbers
 
 -- Tabs
-vim.opt.tabstop = 2 -- Number of spaces that <Tab> in the file counts for. (change only if expandtab = true).
-vim.opt.softtabstop = 2 -- Number of spaces that <Tab> counts for while performing editing.
-vim.opt.shiftwidth = 2 -- Numbers of spaces to use for each step of (auto)indent.
+vim.opt.tabstop = 2      -- Number of spaces that <Tab> in the file counts for. (change only if expandtab = true).
+vim.opt.softtabstop = 2  -- Number of spaces that <Tab> counts for while performing editing.
+vim.opt.shiftwidth = 2   -- Numbers of spaces to use for each step of (auto)indent.
 vim.opt.expandtab = true -- Insert spaces instead of tabs.
 
 -- Search
-vim.opt.hlsearch = true -- Highlight all the matches.
+vim.opt.hlsearch = true  -- Highlight all the matches.
 vim.opt.incsearch = true -- Show incremental matches while typing the search.
 
 --------------------------------------------------------------------------------------------------
@@ -47,12 +48,11 @@ vim.opt.incsearch = true -- Show incremental matches while typing the search.
 packer = require 'packer'
 local use = packer.use
 packer.startup(function()
-
   use {
     'https://codeberg.org/esensar/nvim-dev-container', -- To use Devcontainers (Docker). Docs: https://github.com/esensar/nvim-dev-container
-    requires = { 'nvim-treesitter/nvim-treesitter' } -- Needed  to parse the config
+    --requires = { 'nvim-treesitter/nvim-treesitter' }   -- Needed  to parse the config
   }
-
+  use 'aiken-lang/editor-integration-nvim'
 end)
 
 require("devcontainer").setup {}

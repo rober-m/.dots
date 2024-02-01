75d14cac93c4a52f3de359c4dc3459489385bed4
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-02-01 19:13:36 +0000

    installed global HLS on linux

--- linux/home.nix
+++ linux/home.nix
@@ -29,7 +29,7 @@ in {
     firefox # Firefox browser
     obsidian # Can't use it because of old Electron version :(
     # HLS 2.2.0 doesn't work in MacOS :(
-    #haskell-language-server
+    haskell-language-server
     #(haskell-language-server.override {supportedGhcVersions = ["925" "928"];}) #Also installed in modules/nvim/lsp-config.nix
   ];
 

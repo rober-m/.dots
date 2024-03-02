35d75c2ea947093f656a1269f7ab6b4a65aa843c
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-03-02 12:54:26 +0000

    added twilight config

--- common/modules/neovim/default.nix
+++ common/modules/neovim/default.nix
@@ -38,7 +38,36 @@
     plugins = with pkgs.vimPlugins; [
       copilot-vim # GitHub Copilot
       packer-nvim # This might be related to packerpath error. Always install, just in case
-      twilight-nvim # Dim inactive portions of the code you're editing
+      {
+        plugin = twilight-nvim; # Dim inactive portions of the code you're editing
+        type = "lua";
+        config = ''
+          ------------------------------------ TWILIGHT -------------------------------------------
+          -- Docs: https://github.com/folke/twilight.nvim
+          require('twilight').setup{
+            dimming = {
+              alpha = 0.5, -- amount of dimming
+              -- we try to get the foreground from the highlight groups or fallback color
+              color = { "Normal", "#ffffff" },
+              term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+              inactive = true, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+            },
+            context = 10, -- amount of lines we will try to show around the current line
+            treesitter = true, -- use treesitter when available for the filetype
+            -- treesitter is used to automatically expand the visible text,
+            -- but you can further control the types of nodes that should always be fully expanded
+            expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+              "function",
+              "method",
+              "table",
+              "if_statement",
+            },
+            exclude = {}, -- exclude these filetypes
+          }
+          -----------------------------------------------------------------------------------------
+
+        '';
+      }
     ];
 
     extraPackages = with pkgs; [

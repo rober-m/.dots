bcec496bc116927eddddeccda0c7af931e752ed1
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-03-23 15:48:31 +0000

    extracted megasync overlay

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -28,28 +28,7 @@
 
   # Overlay to update MEGASync to the latest version
   nixpkgs.overlays = [
-    (
-      self: super: let
-        version = "4.11.0.0";
-        srcFlavor = "Linux";
-      in {
-        megasync = super.megasync.overrideAttrs (
-          attrs: {
-            version = version;
-
-            src = super.fetchFromGitHub {
-              owner = "meganz";
-              repo = "MEGAsync";
-              rev = "v${version}_${srcFlavor}";
-              sha256 = "sha256-stL5qgnT141LZuaY7Bo0TqJVdCd9tB0xFQVmfxyYJU4=";
-              fetchSubmodules = true;
-            };
-
-            buildInputs = attrs.buildInputs;
-          }
-        );
-      }
-    )
+    (import ./overlays/magasync.nix)
   ];
 
   # Auto-upgrade system

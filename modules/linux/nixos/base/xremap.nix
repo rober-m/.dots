1319eb10f8e229da2310713b6258faf32d2d634e
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-06-22 17:30:40 +0100

    added warning

--- modules/linux/nixos/base/xremap.nix
+++ modules/linux/nixos/base/xremap.nix
@@ -11,6 +11,12 @@
   users.groups.uinput.members = [user-options.username];
   users.groups.input.members = [user-options.username];
 
+  #---------------------------------------------------------
+  # IMPORTANT!!!: For this to work on the Moonlander, make
+  # sure to plug it in AFTER booting the system or re-plug.
+  # Else, this will conflict with the Moonlander's config!!
+  #---------------------------------------------------------
+
   services.xremap = {
     #withHypr = true; # Work with Hyprlnd
     withWlroots = true; # Work with Wayland

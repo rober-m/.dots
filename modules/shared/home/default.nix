5a303f2fd6989e2596d64f04bd115ca6584b6bd2
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2024-09-28 10:27:43 +0100

    stripecli

--- modules/shared/home/default.nix
+++ modules/shared/home/default.nix
@@ -130,6 +130,7 @@ in {
     shellcheck # Shell script analysis tool (Run shellcheck <script>)
     neovide # Neovim GUI
     #agda (not for now)
+    stripe-cli # CLI to test Stripe integrations
   ];
   #----------------------------- Other config files ----------------------------#
   home.file.".config/neofetch/config.conf".source = ./neofetch.conf;

f22a897997945fd45587107ec35b30e2e2f45624
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2025-01-18 09:49:00 -0300

    todo

--- my-pkgs/default.nix
+++ my-pkgs/default.nix
@@ -1,5 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
+# TODO: Organize custom packages and overlays better.
 {pkgs}: {
   my-dioxus-cli = pkgs.callPackage ../../../my-pkgs/my-dioxus-cli.nix {};
 }

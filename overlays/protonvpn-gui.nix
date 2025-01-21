07d8fb1d40b7eb2a3de9f10d5741eb89025c5600
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2025-01-21 14:09:51 +0100

    added protonvpn overlay - It works now!

--- overlays/protonvpn-gui.nix
+++ overlays/protonvpn-gui.nix
@@ -1,5 +1,6 @@
 self: super: let
-  version = "4.6.0";
+  #version = "4.6.0";
+  version = "4.7.3";
 in {
   protonvpn-gui = super.protonvpn-gui.overrideAttrs (
     attrs: {
@@ -9,7 +10,8 @@ in {
         owner = "ProtonVPN";
         repo = "proton-vpn-gtk-app";
         rev = "refs/tags/v${version}";
-        hash = "sha256-GCfr6x0KbIJr2r4UcFtMjuyHZVyDLKPvgtjdpTCb5Ro=";
+        #hash = "sha256-GCfr6x0KbIJr2r4UcFtMjuyHZVyDLKPvgtjdpTCb5Ro=";
+        hash = "sha256-LcHloc4iHDgP6RCwFvJM6GfzTRd3RqYo0medjEhmj6M=";
       };
     }
   );

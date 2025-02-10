e71bfac6fc8f46cfc19a6ad822b947c6f24c4160
Robertino Martinez <48034748+rober-m@users.noreply.github.com>
2025-02-10 20:27:00 +0000

    unified karabiner parameters

--- modules/darwin/home/default.nix
+++ modules/darwin/home/default.nix
@@ -41,6 +41,7 @@ in {
     #inputs.aiken_flake_20.packages.aarch64-darwin.aiken # Aiken CLI
     # darwin.xcode_14_1 # Nix can't install Xcode. Using App Store instead.
     #cardano-pkgs.aiken # Aiken CLI
+    #haskell.compiler.ghc925 #(GHC: 9.2.5  && base: 4.16.4.0)
   ];
 
   # ------------------------------ Misc configuration files ---------------------------------------
